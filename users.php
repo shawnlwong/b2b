33a6fd2ea7648a03c71023019777c4665f9ae5ea
Daniel Sologuren <daniel.sologuren@gmail.com>
2012-02-26 16:15:27 -0500

    this sucks

---
+++ users.php
@@ -0,0 +1,14 @@
+<?php
+echo "Below are the users that are currently in the system.<br/>";
+
+$con = mysqli_connect("htpp://localhost:8888","root","root");
+if (!$con)
+  {
+  die('Could not connect: ' . mysqli_error());
+  }
+
+
+
+
+
+?>
